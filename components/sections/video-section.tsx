d1477f2fc07ee1a7ab38cca2f2948515a9139652
Mikeyjpk <mikey.kerr@truescope.com>
2024-02-10 19:50:35 +1100

    Updated video

--- components/sections/video-section.tsx
+++ components/sections/video-section.tsx
@@ -1,7 +1,10 @@
 const Video = () => {
     return ( 
-        <div className="flex justify-center bg-black/10">
-            <video width="320" height="240" autoPlay muted loop>         
+        <div className="flex justify-center relative items-center pt-10">
+            <video width="360" height="240" autoPlay muted loop className="blur-lg opacity-80">      
+                <source src="./video/ampm-video.mp4" type="video/mp4" />      
+            </video>  
+            <video width="320" height="240" autoPlay muted loop className="absolute">         
                 <source src="./video/ampm-video.mp4" type="video/mp4" />      
             </video>  
         </div>
