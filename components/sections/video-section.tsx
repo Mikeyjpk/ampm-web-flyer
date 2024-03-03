0a33a3afc31bd01d71cd2f2dad8bb3b02471e3eb
Mikeyjpk <mikey.kerr@truescope.com>
2024-03-03 12:13:12 +1100

    added loading thumbnail to video

--- components/sections/video-section.tsx
+++ components/sections/video-section.tsx
@@ -1,7 +1,14 @@
 const VideoSection = async () => {
     return ( 
         <div className="flex justify-center items-center">
-            <video  playsInline autoPlay muted loop controls className="">      
+            <video 
+                playsInline 
+                autoPlay 
+                muted 
+                loop 
+                controls
+                poster="./images/Thumbnail_AMPM.jpg"
+            >      
                 <source src="./video/ampm-video_1440p.mp4" type="video/mp4" />    
             </video>  
         </div>
