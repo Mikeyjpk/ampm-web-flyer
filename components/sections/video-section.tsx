b8f192b700bc8a8cbb52dbedd936b59b20aef71c
Mikeyjpk <mikey.kerr@truescope.com>
2024-03-03 12:05:04 +1100

    add controls to video

--- components/sections/video-section.tsx
+++ components/sections/video-section.tsx
@@ -1,7 +1,7 @@
 const VideoSection = async () => {
     return ( 
         <div className="flex justify-center items-center">
-            <video  playsInline autoPlay muted loop className="">      
+            <video  playsInline autoPlay muted loop controls className="">      
                 <source src="./video/ampm-video_1440p.mp4" type="video/mp4" />    
             </video>  
         </div>
