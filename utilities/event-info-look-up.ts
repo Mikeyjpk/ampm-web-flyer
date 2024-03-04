9530ebeb46b44342380d19283f2d98ff29cc687f
Mikeyjpk <mikey.kerr@truescope.com>
2024-03-04 17:24:53 +1100

    add ticket links

--- utilities/event-info-look-up.ts
+++ utilities/event-info-look-up.ts
@@ -3,7 +3,7 @@ export const eventInfo = {
     date: "Fri 26th April",
     city: "Newcastle",
     location: "Newcastle Hotel",
-    ticketLink: "https://www.eventbrite.com.au/",
+    ticketLink: "https://www.eventbrite.com.au/e/852042771867",
     eventLink:
       "https://www.facebook.com/events/s/ampm-emo-night-sydney-good-fri/1744826922677244/?mibextid=9l3rBW",
   },
@@ -11,7 +11,7 @@ export const eventInfo = {
     date: "Sat 27th April",
     city: "Sydney",
     location: "Burdekin Hotel",
-    ticketLink: "https://www.eventbrite.com.au/",
+    ticketLink: "https://www.eventbrite.com/e/855612539137",
     eventLink:
       "https://www.facebook.com/events/s/ampm-emo-night-sydney-good-fri/1744826922677244/?mibextid=9l3rBW",
   },
@@ -19,7 +19,7 @@ export const eventInfo = {
     date: "Fri 3rd	May",
     city: "Canberra",
     location: "Transit Bar",
-    ticketLink: "https://www.eventbrite.com.au/",
+    ticketLink: "https://www.eventbrite.com/e/855660592867",
     eventLink:
       "https://www.facebook.com/events/s/ampm-emo-night-sydney-good-fri/1744826922677244/?mibextid=9l3rBW",
   },
@@ -27,7 +27,7 @@ export const eventInfo = {
     date: "Sat 4th May",
     city: "Penrith",
     location: "Tattersalls Hotel",
-    ticketLink: "https://www.eventbrite.com.au/",
+    ticketLink: "https://www.eventbrite.com/e/855664043187",
     eventLink:
       "https://www.facebook.com/events/s/ampm-emo-night-sydney-good-fri/1744826922677244/?mibextid=9l3rBW",
   },
@@ -35,7 +35,8 @@ export const eventInfo = {
     date: "Fri 10th May",
     city: "Albury",
     location: "Beer Deluxe",
-    ticketLink: "https://www.eventbrite.com.au/",
+    ticketLink:
+      "https://tickets.oztix.com.au/outlet/event/7654ca43-c80e-418e-8abe-72533945ed26",
     eventLink:
       "https://www.facebook.com/events/s/ampm-emo-night-sydney-good-fri/1744826922677244/?mibextid=9l3rBW",
   },
@@ -43,7 +44,7 @@ export const eventInfo = {
     date: "Sat 11th May",
     city: "Melbourne",
     location: "Inflation Nightclub",
-    ticketLink: "https://www.eventbrite.com.au/",
+    ticketLink: "https://www.eventbrite.com/e/855665477477",
     eventLink:
       "https://www.facebook.com/events/s/ampm-emo-night-sydney-good-fri/1744826922677244/?mibextid=9l3rBW",
   },
@@ -51,7 +52,7 @@ export const eventInfo = {
     date: "Fri 17th May",
     city: "Brisbane",
     location: "The Brightside",
-    ticketLink: "https://www.eventbrite.com.au/",
+    ticketLink: "https://www.eventbrite.com.au/e/855667593807",
     eventLink:
       "https://www.facebook.com/events/s/ampm-emo-night-sydney-good-fri/1744826922677244/?mibextid=9l3rBW",
   },
@@ -59,7 +60,8 @@ export const eventInfo = {
     date: "Sat 18th May",
     city: "Gold Coast",
     location: "Vinnies Dive Bar",
-    ticketLink: "https://www.eventbrite.com.au/",
+    ticketLink:
+      "https://vinniesdive.oztix.com.au/outlet/event/14e47e41-1280-447e-b5d4-94e82dbd3132",
     eventLink:
       "https://www.facebook.com/events/s/ampm-emo-night-sydney-good-fri/1744826922677244/?mibextid=9l3rBW",
   },
