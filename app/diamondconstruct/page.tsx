4e51fe5c4d296aa678ba1cca45c5e754106afc67
Mikeyjpk <mikey.kerr@truescope.com>
2024-03-04 15:44:46 +1100

    add route

---
+++ app/diamondconstruct/page.tsx
@@ -0,0 +1,27 @@
+import { Suspense } from "react";
+
+import Container from "@/components/container";
+import EventInfo from "@/components/sections/event-info-section";
+import TicketSection from "@/components/sections/ticket-link-section";
+import Footer from "@/components/sections/footer";
+import HeroSection from "@/components/sections/hero-section";
+import TitleSection from "@/components/sections/title-section";
+import VideoSection from "@/components/sections/video-section";
+import Loading from "@/components/loading";
+
+export default function TourPage() {
+  return (
+    <main>
+        <Container>
+          <HeroSection />
+          <TicketSection />
+          <Suspense fallback={<Loading />}>
+            <VideoSection />
+          </Suspense>
+          <TitleSection />
+          <EventInfo />
+        </Container>
+      <Footer />
+    </main>
+  );
+}
